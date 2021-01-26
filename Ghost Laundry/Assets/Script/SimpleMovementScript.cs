2f4639d7d4d31d8824bf8d8f883ffdd6ba4b6809
Sofiane <sofiane.teron@umontreal.ca>
2021-01-26 19:40:22 +0100

    Mise en place de quatres zones
    pour set-up les mouvements de caméra (peek et changement de points de vue on trigger)

---
+++ Ghost Laundry/Assets/Script/SimpleMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovementScript : MonoBehaviour
+{
+    public float moveSpeed; 
+
+   
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //je déclare mes axes 
+        float X = Input.GetAxis("Horizontal");
+        float Y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(X,Y,0) * Time.deltaTime * moveSpeed; 
+    }
+}
