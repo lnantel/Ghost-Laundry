f0f42f052c1f35dc47ae1cb1a29a38ca9baa49c9
Sofiane <sofiane.teron@umontreal.ca>
2021-01-28 13:56:53 +0100

    Cam & Peek system : fixed
    Smooth &  Minor bug correction to do

---
+++ Ghost Laundry/Assets/Script/CameraSwitch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public GameObject ghost;
+
+   
+
+  
+
+    public float lerpTime;
+
+   
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+  
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+public void Move(Vector2 destination){
+
+    
+    transform.position = Vector2.Lerp(transform.position, destination,lerpTime);
+    
+
+}
+
+public void Peek(Vector2 peekdestination){
+
+    
+    transform.position = Vector2.Lerp(transform.position, peekdestination,lerpTime);
+
+}
+
+
+  
+}
---
+++ Ghost Laundry/Assets/Script/RoomTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+
+    public CameraSwitch cameraSwitch;
+    public GameObject ghost; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D ghostColl){
+
+        if(ghostColl.gameObject.tag == "Player"){
+
+            cameraSwitch.Move(transform.position); 
+        }
+}
+
+}
