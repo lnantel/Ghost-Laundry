370ebfd7a21b8cc3591edcaf3857c8f6a8d22010
Louis Nantel <nantel.louis@gmail.com>
2021-01-30 13:59:21 -0500

    Set up animation controller

---
+++ Ghost Laundry/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private PlayerStateManager state;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = PlayerStateManager.instance;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("Dashing", state.Dashing);
+        animator.SetBool("Carrying", state.Carrying);
+        animator.SetBool("Walking", state.Walking);
+    }
+}
