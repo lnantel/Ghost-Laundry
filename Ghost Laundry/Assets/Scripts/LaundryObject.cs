922f7d288d13eac0d39e11c4d75ba97c70d8672e
Louis Nantel <nantel.louis@gmail.com>
2021-02-01 22:01:17 -0500

    Added LaundryObject, implemented click & drag

---
+++ Ghost Laundry/Assets/Scripts/LaundryObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaundryObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void OnInteract() {
+        Debug.Log(gameObject.name + " interaction");
+    }
+
+    public virtual void OnGrab() {
+        Debug.Log(gameObject.name + " grabbed");
+    }
+
+    public virtual void OnRelease() {
+        Debug.Log(gameObject.name + " released");
+    }
+}
--- Ghost Laundry/Assets/Scripts/LaundryTaskController.cs
+++ Ghost Laundry/Assets/Scripts/LaundryTaskController.cs
@@ -6,8 +6,10 @@ public class LaundryTaskController : MonoBehaviour
 {
     public float CursorSensitivity;
     public float CursorSpeed;
-    public Rigidbody2D cursor;
+    public Transform cursor;
     private float snappiness = 50.0f;
+    private Vector2 worldBottomLeft;
+    private Vector2 worldTopRight;
 
     private bool interactInput;
     private bool interactInputHeld;
@@ -16,7 +18,7 @@ public class LaundryTaskController : MonoBehaviour
     private float moveYInput;
     private bool backInput;
 
-    private bool objectGrabbed;
+    private LaundryObject grabbedObject;
 
     private IEnumerator DelayGrabCoroutine;
     private float grabDelay = 0.15f;
@@ -24,7 +26,12 @@ public class LaundryTaskController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
 
+    private void OnEnable() {
+        worldBottomLeft = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, -Camera.main.transform.position.z));
+        worldTopRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, -Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
@@ -39,9 +46,16 @@ public class LaundryTaskController : MonoBehaviour
         moveXInput = Mathf.Lerp(moveXInput, Input.GetAxis("TaskHorizontal"), snappiness * Time.deltaTime);
         moveYInput = Mathf.Lerp(moveYInput, Input.GetAxis("TaskVertical"), snappiness * Time.deltaTime);
         Vector2 moveInput = new Vector2(moveXInput, moveYInput);
+        Vector2 displacement = moveInput * CursorSensitivity * CursorSpeed;
 
-        //Cursor Movement
-        cursor.velocity = moveInput * CursorSensitivity * CursorSpeed;
+        //Cursor movement
+        if(displacement.magnitude > 0.0006f) //accounts for floating point uncertainty; prevents drift
+            cursor.position += new Vector3(displacement.x, displacement.y, 0.0f);
+
+        //Cursor clamping
+        float clampedX = Mathf.Clamp(cursor.position.x, worldBottomLeft.x, worldTopRight.x);
+        float clampedY = Mathf.Clamp(cursor.position.y, worldBottomLeft.y, worldTopRight.y);
+        cursor.position = new Vector2(clampedX, clampedY);
 
         //Interact vs Grab
         if (interactInput) {
@@ -50,38 +64,57 @@ public class LaundryTaskController : MonoBehaviour
             StartCoroutine(DelayGrabCoroutine);
         }
 
+        //Drag grabbed object
+        if (grabbedObject != null)
+            grabbedObject.transform.position = cursor.position;
+
         //Grab & Release
-        if(objectGrabbed && !interactInputHeld) {
+        if(grabbedObject != null && !interactInputHeld) {
             Release();
         }
     }
 
     private IEnumerator DelayGrab() {
-        float timer = 0.0f;
-        while(timer < grabDelay) {
-            yield return new WaitForSeconds(0.0f);
-            timer += Time.deltaTime;
-            if (!interactInputHeld) break;
+        //Detects the targeted LaundryObject
+        LaundryObject target = null;
+        int layerMask = LayerMask.GetMask("LaundryObject");
+        Collider2D col = Physics2D.OverlapCircle(cursor.position, 0.1f, layerMask);
+        if (col != null) {
+            target = col.GetComponent<LaundryObject>();
         }
-        if (interactInputHeld) Grab();
-        else Interact();
+        if(target != null) {
+            //If the cursor moves more than 0.1f units while interact is held, or if it is held for longer than grabDelay,
+            //then the input is interpreted as a grab rather than an interact.
+            //Otherwise, on release, the input is interpreted as an interact.
+            float timer = 0.0f;
+            Vector2 initialCursorPosition = cursor.position;
+            float cursorDelta;
+            while (timer < grabDelay) {
+                yield return new WaitForSeconds(0.0f);
+                timer += Time.deltaTime;
+                cursorDelta = (initialCursorPosition - new Vector2(cursor.position.x, cursor.position.y)).magnitude;
+                if (!interactInputHeld || cursorDelta > 0.1f) break;
+            }
+            if (interactInputHeld) Grab(target);
+            else Interact(target);
+        }
         DelayGrabCoroutine = null;
     }
 
-    private void Interact() {
-        Debug.Log("Interact");
-        //Detect an Interactable
+    private void Interact(LaundryObject obj) {
         //Trigger its interaction action
+        obj.OnInteract();
     }
 
-    private void Grab() {
-        Debug.Log("Grab");
-        //Detect a Grabbable
+    private void Grab(LaundryObject obj) {
         //Grab it
+        grabbedObject = obj;
+        obj.OnGrab();
     }
 
     private void Release() {
-        Debug.Log("Release");
+        grabbedObject.OnRelease();
+        grabbedObject = null;
         //Release a grabbed object
     }
 }
