9eeac65552563fac43f0a3a4fbb5ad9966df511c
Louis Nantel <nantel.louis@gmail.com>
2021-02-08 21:43:53 -0500

    Configured SettingsManager to use PlayerPrefs

--- Ghost Laundry/Assets/Scripts/SettingsManager.cs
+++ Ghost Laundry/Assets/Scripts/SettingsManager.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SettingsManager : MonoBehaviour {
+    public static SettingsManager instance;
+
+    public float MouseSensitivity;
+    public float MusicVolume;
+    public float SFXVolume;
+
+    private void Awake() {
+        if (instance != null) Destroy(gameObject);
+        else instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnEnable() {
+        if (PlayerPrefs.HasKey(Settings.MouseSensitivity.ToString()))
+            MouseSensitivity = PlayerPrefs.GetFloat(Settings.MouseSensitivity.ToString());
+        else
+            MouseSensitivity = 0.5f;
+
+        if (PlayerPrefs.HasKey(Settings.MusicVolume.ToString()))
+            MusicVolume = PlayerPrefs.GetFloat(Settings.MusicVolume.ToString());
+        else
+            MusicVolume = 0.5f;
+
+        if (PlayerPrefs.HasKey(Settings.SFXVolume.ToString()))
+            SFXVolume = PlayerPrefs.GetFloat(Settings.SFXVolume.ToString());
+        else
+            SFXVolume = 0.5f;
     }
+
+    private void OnDisable() {
+        PlayerPrefs.SetFloat(Settings.MouseSensitivity.ToString(), MouseSensitivity);
+        PlayerPrefs.SetFloat(Settings.MusicVolume.ToString(), MusicVolume);
+        PlayerPrefs.SetFloat(Settings.SFXVolume.ToString(), SFXVolume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetMouseSensitivity(float value) {
+        MouseSensitivity = value;
+    }
+
+    public void SetMusicVolume(float value) {
+        MusicVolume = value;
+    }
+
+    public void SetSFXVolume(float value) {
+        SFXVolume = value;
+    }
+}
+
+public enum Settings {
+    MouseSensitivity,
+    MusicVolume,
+    SFXVolume
 }
