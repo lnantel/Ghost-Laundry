e4a54c2abace9d3bc9649992f968c28d14611e0d
Sofiane <sofiane.teron@umontreal.ca>
2021-02-16 20:17:45 -0400

    Intégration des premiers assets de table

---
+++ Ghost Laundry/Assets/Scripts/IronOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronOnClick : MonoBehaviour
+{
+
+    public ParticleSystem particle; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButtonDown(0)){
+
+            particle.Play(); 
+        }
+
+         if(Input.GetMouseButtonUp(0)){
+
+            particle.Stop(); 
+        }
+
+
+    }
+}
