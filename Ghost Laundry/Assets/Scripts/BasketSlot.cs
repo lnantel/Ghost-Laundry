2e036c1ddd880ebd0e9ae0d649f62a3992de9e5b
Louis Nantel <nantel.louis@gmail.com>
2021-02-18 11:35:35 -0500

    Reworked BasketSlots

--- Ghost Laundry/Assets/Scripts/BasketSlot.cs
+++ Ghost Laundry/Assets/Scripts/BasketSlot.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class BasketSlot
 {
     public LaundryBasket laundryBasket;
     public Vector3 spawnPoint;
+
+    public BasketSlot(LaundryBasket laundryBasket, Vector3 spawnPoint) {
+        this.laundryBasket = laundryBasket;
+        this.spawnPoint = spawnPoint;
+    }
 }
