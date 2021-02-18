2e71398817d71a76636ab38b9e686fc5c0a84053
Louis Nantel <nantel.louis@gmail.com>
2021-02-18 13:39:52 -0500

    Added GarmentColor static class & white/colored state to Garment

--- Ghost Laundry/Assets/Scripts/Garment.cs
+++ Ghost Laundry/Assets/Scripts/Garment.cs
@@ -37,4 +37,14 @@ public class Garment
     public void Fold() {
         currentFoldingStep = (currentFoldingStep + 1) % (foldingSteps + 1);
     }
+
+    public bool Colored() {
+        return !(color == GarmentColor.White);
+    }
+}
+
+public static class GarmentColor {
+    public static Color White = Color.white;
+    public static Color Red = Color.red;
+    public static Color Pink = new Color(1.0f, 0.0f, 1.0f);
 }
