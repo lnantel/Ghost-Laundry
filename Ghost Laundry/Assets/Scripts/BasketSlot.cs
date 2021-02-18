080a0f35ce00d964175b3b5dabda45f3a12fe214
Louis Nantel <nantel.louis@gmail.com>
2021-02-18 10:44:18 -0500

    Created BasketSlots

---
+++ Ghost Laundry/Assets/Scripts/BasketSlot.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketSlot
+{
+    public LaundryBasket laundryBasket;
+    public Vector3 spawnPoint;
+}
