9f325ac925996a7e935750c62fcd3a6ab5416b82
Louis Nantel <nantel.louis@gmail.com>
2021-02-27 12:20:06 -0500

    Implemented a sprite sorting solution

---
+++ Ghost Laundry/Assets/Scripts/LaundromatSpriteSort.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+[RequireComponent(typeof(SortingGroup))]
+public class LaundromatSpriteSort : MonoBehaviour
+{
+    private SortingGroup sortingGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sortingGroup = GetComponent<SortingGroup>();
+        sortingGroup.sortingLayerID = SortingLayer.NameToID("Entities");
+        sortingGroup.sortingOrder = -(int)(transform.position.y * 100.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sortingGroup.sortingOrder = -(int)(transform.position.y * 100.0f);
+    }
+}
