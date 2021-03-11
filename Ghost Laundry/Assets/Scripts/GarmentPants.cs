6f35273291f4b5f3685d3aad7daabf015a33a205
Louis Nantel <nantel.louis@gmail.com>
2021-03-11 09:29:00 -0500

    Pants embiggened

--- Ghost Laundry/Assets/Scripts/GarmentPants.cs
+++ Ghost Laundry/Assets/Scripts/GarmentPants.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GarmentPants : Garment {
     public GarmentPants(Fabric fabric, Color color, bool clean = false, bool dry = true, bool pressed = false, bool folded = false, bool ruined = false) : base(fabric, color, clean, dry, pressed, folded, ruined) {
-        size = 1;
+        size = 2;
         foldingSteps = 3;
         laundryGarmentPrefab = (GameObject)Resources.Load("LaundryGarmentPants");
     }
