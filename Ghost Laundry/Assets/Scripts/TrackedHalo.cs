d232163993b5d3df55fd78b323eb9f11449c68e6
Louis Nantel <nantel.louis@gmail.com>
2021-03-29 10:24:36 -0400

    Preparing to upgrade to 2D renderer

--- Ghost Laundry/Assets/Scripts/TrackedHalo.cs
+++ Ghost Laundry/Assets/Scripts/TrackedHalo.cs
@@ -6,6 +6,8 @@ public class TrackedHalo : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
 
+    private Material defaultMaterial;
+    private Material haloMaterial;
     private ITrackable Trackable;
 
     private void Start() {
@@ -14,14 +16,17 @@ public class TrackedHalo : MonoBehaviour
             Debug.LogError("No ITrackable found on this GameObject");
             Destroy(this);
         }
+
+        defaultMaterial = spriteRenderer.material;
+        haloMaterial = (Material)Resources.Load("MAT_TrackedMaterial");
     }
 
     private void Update() {
         if (Trackable.ContainsTrackedGarment()) {
-            spriteRenderer.color = Color.green;
+            spriteRenderer.material = haloMaterial;
         }
         else {
-            spriteRenderer.color = Color.white;
+            spriteRenderer.material = defaultMaterial;
         }
     }
 }
