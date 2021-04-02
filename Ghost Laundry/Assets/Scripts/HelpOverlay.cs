d1b84ab448d871ab319426fe07d827cf1a18cca8
Louis Nantel <nantel.louis@gmail.com>
2021-04-02 16:00:26 -0400

    Implemented HelpOverlay skeleton

---
+++ Ghost Laundry/Assets/Scripts/HelpOverlay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpOverlay : MonoBehaviour
+{
+    public GameObject overlay;
+    public bool visible;
+
+    private void Start() {
+        overlay.SetActive(visible);
+    }
+
+    public void ToggleOverlay() {
+        visible = !visible;
+        overlay.SetActive(visible);
+    }
+}
