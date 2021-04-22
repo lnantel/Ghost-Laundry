ebbb80c4a82ebe863451a97fc8b855e2e08ec360
Louis Nantel <nantel.louis@gmail.com>
2021-04-22 10:47:00 -0400

    Set up WM+Dryer shadows and proper Dryer shake animation

---
+++ Ghost Laundry/Assets/Scripts/DryerShadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DryerShadow : MonoBehaviour
+{
+    public Transform DryerRenderer;
+    private Vector3 idlePosition;
+
+    private void Start() {
+        idlePosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(DryerRenderer.localPosition.x, idlePosition.y, idlePosition.z);
+    }
+}
