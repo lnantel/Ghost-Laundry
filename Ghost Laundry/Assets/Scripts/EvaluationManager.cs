25b504fd3f89a23d9ff96540f3541ccc46665239
Louis Nantel <nantel.louis@gmail.com>
2021-04-22 13:13:26 -0400

    Fixed Eval screen not showing up on failure or when money == 0.

--- Ghost Laundry/Assets/Scripts/EvaluationManager.cs
+++ Ghost Laundry/Assets/Scripts/EvaluationManager.cs
@@ -55,7 +55,7 @@ public class EvaluationManager : MonoBehaviour
     }
 
     private void OnShowEvaluation() {
-        if(MoneyManager.instance.CurrentAmount > 0) {
+        if(MoneyManager.instance.CurrentAmount >= 0) {
             UpdateSuccessText();
             success.SetActive(true);
         }
