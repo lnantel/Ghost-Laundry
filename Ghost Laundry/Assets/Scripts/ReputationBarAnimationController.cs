afd6bde1dbb250e348b9c8587ca112870f305dbd
Louis Nantel <nantel.louis@gmail.com>
2021-04-24 11:42:38 -0400

    Animated Reputation bar

---
+++ Ghost Laundry/Assets/Scripts/ReputationBarAnimationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReputationBarAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable() {
+        FlyingStar.ReachedDestination += Animate;
+    }
+
+    private void OnDisable() {
+        FlyingStar.ReachedDestination -= Animate;
+    }
+
+    private void Animate(bool sign) {
+        animator.ResetTrigger("StarHit");
+        animator.SetTrigger("StarHit");
+    }
+}
