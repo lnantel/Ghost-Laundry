9dd0c984eae07511d6b80a27e0fe34ea413299e2
Maya <maya.abdel.ahad@umontreal.ca>
2021-05-03 13:31:57 -0400

    Spike: Ajouter les nouveaux sprites des chaussettes

---
+++ Ghost Laundry/Assets/Scripts/Sounds.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Sounds
+{
+    Dash,
+    Collision,
+    MoneyGain,
+    Fold1,
+    Fold2,
+    Fold3,
+    Fold4,
+    SyntheticGrab,
+    SyntheticDrop,
+    CottonGrab,
+    CottonDrop,
+    WoolGrab,
+    WoolDrop,
+    BoneGrab,
+    BoneDrop,
+    Tick,
+    Tock,
+    LaundromatOpening,
+    OllieArrives,
+    OllieWhistle,
+    LaundromatClosing,
+    CustomerArrives,
+    ShirtButton1,
+    ShirtButton2,
+    ShirtButton3,
+    ShirtButton4,
+    ShirtButton5,
+    ShirtButton6,
+    LinenGrab,
+    LinenDrop,
+    SilkGrab,
+    SilkDrop,
+    DenimGrab,
+    DenimDrop,
+
+    //WM, 9 sons
+
+    OpenWMDoor,
+    CloseWMDoor,
+    RunningWM,
+    OpenDetergentWM,
+    CloseDetergentWM,
+    PourDetergent,
+    StartButtonWM,
+    SettingButtonWM,
+    EndWMBeep,
+
+    //Dryer 8 sons
+
+    OpenDryerDoor,
+    CloseDryerDoor,
+    RunningDryer,
+    OpenLintTrap,
+    CloseLintTrap,
+    CleanLintTrap,
+    StartButtonDryer,
+    SettingButtonDryer,
+    EndDryerBeep,
+
+    //IronBoard 4 sons
+
+    IronIsOn,
+    IronIsWorking,
+    IronisBurning,
+    ShiningGarment,
+
+    //Bassine 4 sons
+    ScratchGarment, 
+    BubbleFoam,
+    PourSoap,
+    SplashGarment,
+
+
+    //Emballeur 4 sons
+    DropGarmentEmb, 
+    OpenEmbDoor,
+    CloseEmbDoor,
+    ProcessingEmb
+
+
+}
